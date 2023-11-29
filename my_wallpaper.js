7b653e0c5b375b48296a4bcac633e39ed1c6d5dd
CharlotteCherry <cherrychar@myvuw.ac.nz>
2023-11-29 21:12:25 +1300

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,32 +1,26 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
 
-let headX = 100;
-let headY = 120;
+let bodyX = 90;
+let bodyY = 115;
 
-let eyesizeX = 10;
-let eyesizeY = 9;
-let eyeY = headY +5;
-let handsH = 20 ;
-let handsW = 15 ;
-let M = 10;
-let earY = headY - 60;
+
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  
+   //pWallpaper.output_mode(DEVELOP_GLYPH );
+   pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 0;
 }
 
 function wallpaper_background() {
-  background(300); //light honeydew green colour
+  background(195,221,243); //light honeydew green colour
 }
 
 
@@ -34,47 +28,53 @@ function wallpaper_background() {
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
  
   
-  //ear
-  fill(71, 102, 155)
-  stroke (71, 102, 155)
-  ellipse(headX - 35, earY, 30, 70); // left ear
-  ellipse(headX + 35,earY, 30, 70);//right ear
   
-  //ear highlight
-  stroke(253,130,140,180);
-  fill (253,130,140,180);
-  ellipse(headX + 35,earY,20,50);
-  ellipse(headX - 35,earY,20,50);
 
-  //head
-  fill(300)
-  stroke(71, 102, 155); 
-  ellipse (headX, headY,105,100);
-  
-// nose and eye/highlights
-  fill(71, 102, 155);
-  stroke (71, 102, 155);
-  ellipse (headX,headY + 15,9,5);// nose 
- ellipse (headX - 20,eyeY,eyesizeX,eyesizeY);//left eye 
- ellipse (headX + 20,eyeY,eyesizeX,eyesizeY); //right eye 
+
+  //head 
+  fill(0);
+  stroke(0);
+  ellipse (120,90, 50,50);
  
- fill(300);
-stroke (300,155);
-ellipse (headX - 20,eyeY,eyesizeX - 9,eyesizeY - 8);//left eye highlight
-ellipse (headX + 20,eyeY,eyesizeX - 9,eyesizeY - 8); //right eye highlight
 
- //blush
- stroke(300);
- fill (253,130,140,100);
- ellipse (headX + 30,headY + 20,30,20);
-ellipse (headX - 30,headY + 20,30,20);// blush 
+  //body
+  fill(241, 94, 87)
+  stroke(241, 94, 87); 
+  ellipse (bodyX, bodyY,80,80);
 
-// hands and mouth
-  fill(300);
-  stroke(71, 102, 155); 
-  ellipse (headX + 30,headY + 40,handsH,handsW);// hands 
-  ellipse (headX - 30,headY + 40,handsH,handsW);
-  ellipse (headX,headY + 30,M,M);// mouth 
+  // spots 
+  fill (0);
+  stroke (0);
+  ellipse ( 90, 100, 10,10);
+  ellipse (115, 110, 15, 15);
+
+  // leafs 
+
+fill (150, 173, 127);
+stroke (150, 173, 127);
+ellipse (40, 0, 50, 35);// left upper corner 
+  
+ 
+fill (150, 173, 127);
+stroke (150, 173, 127);
+ellipse (200,44,35,50);// righ upper corner
+
+ fill (107,144,119);
+stroke (107,144,119);
+  ellipse (155,200,50,35);// righ botom corner 
+
+// left bottom corner
+ellipse (0,155,35,50)
+  
+fill (300);// circle 
+stroke (300);
+ellipse (0,0,20,20);
+ 
+fill (0);
+line (70,150,70,150);
+ 
+
+
 
 }
 
