741914a2fc23d124d6f36b7f57fcb4e9bd24ff9e
CharlotteCherry <cherrychar@myvuw.ac.nz>
2023-11-30 22:23:07 +1300

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -12,7 +12,9 @@ let circleX = 200;
 let circleY = 200;
 let size = circleX - 180;
 
-let
+let petalW = 35 // 35
+let petalH = 50 // 50
+
 
 function setup_wallpaper(pWallpaper) {
   
@@ -30,8 +32,6 @@ function setup_wallpaper(pWallpaper) {
 function wallpaper_background() {
   background(195,221,243); //light blue 
 }
-
-
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
  
@@ -71,23 +71,22 @@ ellipse (bodyX + 55, bodyY - 55, 5, 5);
 
 fill (150, 173, 127);// light green
 stroke (150, 173, 127);
-ellipse (40, 0, 50, 35);// left upper  
-ellipse (200,44,35,50);// righ upper 
-ellipse (200, 155, 35, 50);
+ellipse (40, 0, petalH, petalW);// left upper  
+ellipse (200,44, petalW,petalH);// righ upper 
+ellipse (200, 155, petalW, petalH);
  
 fill (107,144,119);// dark green
 stroke (107,144,119);
-  ellipse (155,200,50,35);// righ botom 
-  ellipse (0, 45, 35,50);// right upper
-  ellipse (0,155,35,50)// left bottom
+  ellipse (155,200,petalH,petalW);// righ botom 
+  ellipse (0, 45, petalW,petalH);// right upper
+  ellipse (0,155,petalW,petalH)// left bottom
   
-fill (300);// circle for flower
+  fill (300);// circle for flower
 stroke (300);
 ellipse (circleX - 200, circleY - 200,size,size);
 ellipse (circleX,circleY,size,size);
  
-fill (0);
-//line (70,150,70,150);
+
  
 
 
